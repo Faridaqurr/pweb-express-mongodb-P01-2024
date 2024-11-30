aaf896ae2489cc067e265b32b4e4de6db83a3044
Faridaqurr <150933246+Faridaqurr@users.noreply.github.com>
2024-11-30 12:25:53 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import cors from 'cors'; // Import CORS middleware
+import cors from 'cors';
 import connectDB from './db-connection';
 import authRoutes from './routes/auth.route';
 import bookRouter from './routes/book.route';
@@ -7,22 +7,18 @@ import mechanismRouter from './routes/mechanism.route';
 
 const app = express();
 
-// Gunakan middleware CORS
 app.use(
   cors({
-    origin: 'http://localhost:5173', // Ganti dengan domain frontend Anda
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Metode HTTP yang diizinkan
-    allowedHeaders: ['Content-Type', 'Authorization'], // Header yang diizinkan
+    origin: 'http://localhost:5173', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
   })
 );
 
-// Middleware untuk parsing JSON
 app.use(express.json());
 
-// Koneksi ke database
 connectDB();
 
-// Endpoint utama
 app.get("/", (_, response) => {
   const options: Intl.DateTimeFormatOptions = {
     weekday: 'long',
@@ -42,12 +38,10 @@ app.get("/", (_, response) => {
   });
 });
 
-// Rute lain
 app.use("/book", bookRouter);
 app.use('/auth', authRoutes);
 app.use('/mechanism', mechanismRouter);
 
-// Jalankan server
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Express is running on Port ${PORT}`);
